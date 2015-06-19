2f69407e2a35d6047e8d42a67d0bbaf5caf139d7
caoyun Zhan <jamesecy@yahoo.ca>
2015-06-18 19:16:54 -0700

    dfdgfdg

--- TodoWebApp1/Global.asax.cs
+++ TodoWebApp1/Global.asax.cs
@@ -10,7 +10,7 @@ namespace TodoWebApp1
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        protected void Application_Start()
+        protected void Application_Start1()
         {
             //sdgfdfgdf
             AreaRegistration.RegisterAllAreas();
